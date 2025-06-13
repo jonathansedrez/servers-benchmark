ed7e2154ec0d932b19dcf55c827c2c933f2c2381
Jonathan Sedrez <jonathansedrez@gmail.com>
2025-06-13 08:55:40 -0300

    refactor: update run-deno-hono.sh for correct linux settings

--- gatling-benchmarks/run-deno-hono.sh
+++ gatling-benchmarks/run-deno-hono.sh
@@ -1,8 +1,7 @@
-#!/bin/bash
+#!/usr/bin/bash
 
-ulimit -n 65535
-sudo sysctl -w net.inet.ip.portrange.first=1025
-sudo sysctl -w net.inet.ip.portrange.last=65535
+ulimit -n 65000
+sudo sysctl -w net.ipv4.ip_local_port_range="1025 65535"
 echo 300000 | sudo tee /proc/sys/fs/nr_open
 echo 300000 | sudo tee /proc/sys/fs/file-max
 
